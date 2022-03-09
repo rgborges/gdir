8646d26f9fd3eedb836761dedc3851c1311bba74
Rafael Borges <rgborges96@gmail.com>
2022-03-08 23:48:28 -0300

    equalizing commits

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories(@"stores");
+
+foreach (var dir in listOfDirectories)
+{
+    Console.WriteLine(dir);
+}
+IEnumerable<string> files = Directory.EnumerateFiles("stores");
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
+
+// Outputs:
+// stores/totals.txt
+// stores/sales.json
